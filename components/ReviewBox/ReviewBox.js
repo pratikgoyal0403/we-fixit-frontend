18bd054fa0c50e2d0d38023de80fe2ee4056f15f
paras594 <parasarora594@gmail.com>
2022-01-23 13:09:22 +0530

    made service-page responsive

--- components/ReviewBox/ReviewBox.js
+++ components/ReviewBox/ReviewBox.js
@@ -1,10 +1,10 @@
 import Review from "./Review";
 
 export default function ReviewBox({ reviews }) {
-  return (
-    <div className="w-3/4 my-5">
-      {reviews &&
-        reviews.map((review) => <Review key={review._id} {...review} />)}
-    </div>
-  );
+	return (
+		<div className="2xl:w-3/4">
+			{reviews &&
+				reviews.map((review) => <Review key={review._id} {...review} />)}
+		</div>
+	);
 }
